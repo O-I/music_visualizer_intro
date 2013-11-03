569ae076444b27efc6857350ee1179f8505da83b
Rahul Horé <hore.rahul@gmail.com>
2013-11-02 21:43:26 -0400

    Creates visualizer.rb

---
+++ visualizer.rb
@@ -0,0 +1,16 @@
+# Visualizer
+
+class Visualizer < Processing::App
+
+  def setup
+    smooth            # smoother == prettier
+    size(1280, 100)   # let's pick a more interesting size
+    background 10     # ...and a darker background color
+  end
+  
+  def draw
+  end
+  
+end
+
+Visualizer.new title: "Visualizer"
